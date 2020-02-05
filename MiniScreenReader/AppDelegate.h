cee036b6c69080d43b2f64c97f26170f47a02a2e
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-02-05 11:36:12 -0600

    Initial Commit

---
+++ MiniScreenReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiniScreenReader
+//
+//  Created by Nicholas Josephson on 2020-02-05.
+//  Copyright © 2020 Nicholas Josephson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MiniScreenReader/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  MiniScreenReader
+//
+//  Created by Nicholas Josephson on 2020-02-05.
+//  Copyright © 2020 Nicholas Josephson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
