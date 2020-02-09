c3796880f7f510cec19ae539ceab024bc893e22a
Nicholas Josephson <josephs6@myumanitoba.ca>
2020-02-08 20:26:53 -0600

    Get started

--- MiniScreenReader/MainWindow.m
+++ MiniScreenReader/MainWindow.m
@@ -20,6 +20,35 @@
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    
+    NSDictionary *options = @{
+        (__bridge NSString*)kAXTrustedCheckOptionPrompt : @YES,
+    };
+    BOOL isProcessTrusted = AXIsProcessTrustedWithOptions((CFDictionaryRef)options);
+    
+    NSLog(@"Processed is trusted: %d", isProcessTrusted);
+    
+    
+    AXUIElementRef systemWide = AXUIElementCreateSystemWide();
+    
+    
+    CFShow(systemWide);
+    
+
+//    CFStringRef keys[3];
+//    CFTypeRef values[3];
+//    CFDictionaryRef options;
+//    keys[0] = kAXTrustedCheckOptionPrompt;
+//    values[0] = kCFBooleanTrue;
+//
+//    options = CFDictionaryCreate(NULL, (void **)keys, (void **)values, 1, &kCFCopyStringDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+//
+//    CFShow(options);
+//
+//    BOOL isProcessTrusted = AXIsProcessTrustedWithOptions(options);
+//      NSLog(@"Processed is trusted: %d", isProcessTrusted);
+//
+//    CFRelease(options);
 }
 
 @end
